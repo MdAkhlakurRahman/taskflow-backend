aa68c438ec3be8fb289dad517fe8b6540a004fd2
MdAkhlakurRahman <coolakhlakur10@gmail.com>
2026-05-18 09:56:11 +0530

    [TICKET-6] Introduce centralized user mapper

---
+++ src/main/java/com/taskflow/demo/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.taskflow.demo.mapper;
+
+import com.taskflow.demo.dto.UserRequestDTO;
+import com.taskflow.demo.dto.UserResponseDTO;
+import com.taskflow.demo.entity.User;
+
+public class UserMapper {
+    public static User userRequestDTOToUser(UserRequestDTO userRequestDTO){
+        return new User(userRequestDTO.name(), userRequestDTO.email());
+    }
+
+    public static UserResponseDTO userToResponseDTO(User user){
+        return new UserResponseDTO(user.getId(), user.getName(), user.getEmail());
+    }
+}
+

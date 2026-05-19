1cb35ffa32401a2412dc3500c019e4b2bfb735ce
MdAkhlakurRahman <coolakhlakur10@gmail.com>
2026-05-19 20:18:11 +0530

    [TICKET-9] Add input validation architecture

--- src/main/java/com/taskflow/demo/dto/UserResponseDTO.java
+++ src/main/java/com/taskflow/demo/dto/UserResponseDTO.java
@@ -1,5 +1,9 @@
 package com.taskflow.demo.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
 public record UserResponseDTO(Long id, String name,String email) {
 
 }
--- src/main/java/com/taskflow/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/taskflow/demo/exception/GlobalExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.taskflow.demo.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -17,6 +21,22 @@ public class GlobalExceptionHandler {
         Map<String,String> errorResponse = new HashMap<>();
         errorResponse.put("message" , e.getMessage());
         errorResponse.put("timestamp", LocalDateTime.now().toString());
+        return errorResponse;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String,List<String>> methodArgumentNotValidException(MethodArgumentNotValidException e){
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+
+            if(!errorResponse.containsKey(fieldName)){
+                errorResponse.put(fieldName, new ArrayList<>());
+            }
+            errorResponse.get(fieldName).add(message);
+        });
         return errorResponse;
     }
 }
